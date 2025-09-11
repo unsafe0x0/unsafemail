5f7ee296379e2c495221f170a0f4f58016326460
unsafe0x0 <priyannxhuu@gmail.com>
2025-09-11 18:09:58 +0530

    added cors

--- main.go
+++ main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"unsafemail/api"
 	"unsafemail/config"
+
+	"github.com/joho/godotenv"
 )
+
+var corsOrigin string
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment")
+	}
+	corsOrigin = os.Getenv("CORS_ORIGIN")
+	if corsOrigin == "" {
+		corsOrigin = "*"
+	}
+}
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", corsOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
@@ -20,10 +48,10 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/send-email", api.EmailHandler)
 
-	loggedMux := loggingMiddleware(mux)
+	handler := loggingMiddleware(corsMiddleware(mux))
 
 	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", loggedMux); err != nil {
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
 }
